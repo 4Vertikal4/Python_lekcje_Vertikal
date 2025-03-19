ae132393e567699e2b76f89ced342f564c5a29c7
4Vertikal4 <92148157+4Vertikal4@users.noreply.github.com>
2025-03-19 14:34:21 +0100

    Update generator_nazw.py
    Dodanie kodu z odcinka.

---
+++ lekcja-01-generator-nazw/generator_nazw.py
@@ -0,0 +1,4 @@
+print("Witaj w Generatorze Nazw Firm!")
+zwierze = input("Podaj nazwę swojego ulubionego zwierzęcia:\n")
+usługa = input("Jaką usługę oferuje Twoja firma?\n")
+print("Nazwa Twojej firmy to: " + zwierze + " " + usługa)
